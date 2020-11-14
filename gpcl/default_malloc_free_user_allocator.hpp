4c9236d6496cc641367c9bcb991b375a29562021
chingyat <tsingyat@outlook.com>
2020-11-14 18:42:55 +0800

    add singleton_pool<> interface

--- gpcl/default_malloc_free_user_allocator.hpp
+++ gpcl/default_malloc_free_user_allocator.hpp
@@ -1,3 +1,13 @@
+//
+// default_malloc_free_user_allocator.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2020 Zhengyi Fu (tsingyat at outlook dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef GPCL_DEFAULT_MALLOC_FREE_USER_ALLOCATOR_HPP
 #define GPCL_DEFAULT_MALLOC_FREE_USER_ALLOCATOR_HPP
 
--- gpcl/object_pool.hpp
+++ gpcl/object_pool.hpp
@@ -1,3 +1,13 @@
+//
+// object_pool.hpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2020 Zhengyi Fu (tsingyat at outlook dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef GPCL_OBJECT_POOL_HPP
 #define GPCL_OBJECT_POOL_HPP
 
