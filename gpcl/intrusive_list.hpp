fb1671e75458876c0408f3bed1c72597fa737fdc
chingyat <tsingyat@outlook.com>
2020-12-12 16:44:46 +0800

    add copyrigth header

--- gpcl/intrusive_list.hpp
+++ gpcl/intrusive_list.hpp
@@ -1,3 +1,13 @@
+//
+// intrusive_list.hpp
+// ~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2020 Zhengyi Fu (tsingyat at outlook dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef GPCL_INTRUSIVE_LIST_HPP
 #define GPCL_INTRUSIVE_LIST_HPP
 
